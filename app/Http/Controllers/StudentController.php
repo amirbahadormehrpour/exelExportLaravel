0d429f2b9ea35c014c2564d677d059df3b853200
amirbahador <amirbahadormehrpour@gmail.com>
2022-03-03 18:26:33 +0330

    csv-controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,5 +1,5 @@
 <?php
-
+// --------------------- https://docs.laravel-excel.com/ ------------------------------
 namespace App\Http\Controllers;
 
 use App\Exports\StudentExport;
@@ -9,6 +9,8 @@ class StudentController extends Controller
 {
     protected function index()
     {
-        return (new StudentExport)->download('student.xlsx');
+        //return (new StudentExport)->download('student.xlsx');
+        return (new StudentExport)->download('invoices.csv', \Maatwebsite\Excel\Excel::CSV);
+
     }
 }
